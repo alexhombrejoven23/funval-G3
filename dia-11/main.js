fc2217092dc03ebce7e6af7a47d642b136febcfe
alexhombrejoven23 <alexcondorimolle2025@gmail.com>
2025-06-02 12:58:01 -0400

    este comit

---
+++ dia-11/main.js
@@ -0,0 +1,63 @@
+
+
+// 1. Escribe un comentario en una línea
+
+// 2. Escribe un comentario en varias líneas
+/*hola amigos 
+que tal. estamos en crisis
+alguien sabe como volar a mnarte*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let name1 = "alex";
+console.log(typeof name1);
+
+let number = 12;
+console.log(typeof number);
+
+let crisis = true;
+console.log(crisis);
+
+let no_defenida = undefined
+console.log(typeof no_definida);
+
+let europa = null;
+console.log(typeof europa);
+
+let esta_cuidad = Symbol("santa cruz");
+console.log(typeof esta_ciudad);
+
+let numeraso = 9238475928347652038947n;
+console.log(typeof numeraso);
+
+// 4. Imprime por consola el valor de todas las variables
+
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+// 1. este es un comentario
+
+let a = 2
+let b = 3
+
+
+
+
+// OPRERADORES DE COMPARACION 
+let aa = 22;
+let bb = 25;
+let resultado = aa==bb  /*FALSE O TRUE, SOLO COMPARA EL DATO, MAS NO EL TIPO DE DATO*/
+console.log(resultado);
+
+let resultado_1 = aa === bb;
+console.log(resultado_1);
+
+
